b35cb7a7e6fdd8b84d14d97ca1b2b176d95e1264
OmarDIOP0 <odiop6170@gmail.com>
2025-01-14 23:06:06 +0000

    correction du bugs🦠

--- eureka-server/src/main/java/com/omar/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/omar/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,5 @@
 package com.omar.eurekaserver;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
