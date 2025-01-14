1c6a88480c0a4050219e6146fd4cdbbe7ee704a4
OmarDIOP0 <odiop6170@gmail.com>
2025-01-14 22:16:21 +0000

    EurekaServerApplication 🧑

---
+++ eureka-server/src/main/java/com/omar/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,10 @@
+package com.omar.eurekaserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
