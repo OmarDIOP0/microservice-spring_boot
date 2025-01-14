36a9fee6f2cb1681a9b5f7feac928adf038a82ae
OmarDIOP0 <odiop6170@gmail.com>
2025-01-14 20:03:48 +0000

    service customer

--- customer/src/main/java/com/omar/customer/CustomerService.java
+++ customer/src/main/java/com/omar/customer/CustomerService.java
@@ -1,9 +1,15 @@
 package com.omar.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.AllArgsConstructor;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+    private final RestTemplate restTemplate;
+    private final CustomerRepository customerRepository;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         //TODO Auto-generated method stub
@@ -12,6 +18,10 @@ public record CustomerService(CustomerRepository customerRepository) {
             .lastName(request.lastName())
             .email(request.email())
             .build();
+            restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                String.class
+            );
             
         customerRepository.save(customer);
     }
