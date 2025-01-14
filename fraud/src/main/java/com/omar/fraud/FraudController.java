4c6c04dc732a77f429097fc4076bc7e05e4b82f9
OmarDIOP0 <odiop6170@gmail.com>
2025-01-14 19:42:44 +0000

    ajout du lombok

--- fraud/src/main/java/com/omar/fraud/FraudController.java
+++ fraud/src/main/java/com/omar/fraud/FraudController.java
@@ -7,12 +7,10 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/api/v1/fraud")
+@RequestMapping("/api/v1/fraud-check")
 @AllArgsConstructor
 public class FraudController {
     private final FraudCheckService fraudCheckService;
