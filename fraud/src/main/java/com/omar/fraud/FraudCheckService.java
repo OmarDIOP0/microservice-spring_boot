9006578963f277c505a095cb98ee0ec04147f2b3
OmarDIOP0 <odiop6170@gmail.com>
2025-01-14 19:38:42 +0000

    ajout du lombok

--- fraud/src/main/java/com/omar/fraud/FraudCheckService.java
+++ fraud/src/main/java/com/omar/fraud/FraudCheckService.java
@@ -4,13 +4,12 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class FraudCheckService {
     private final FraudCheckHistoryRepositry fraudCheckHistoryRepositry;
-
-    public FraudCheckService(FraudCheckHistoryRepositry fraudCheckHistoryRepositry) {
-        this.fraudCheckHistoryRepositry = fraudCheckHistoryRepositry;
-    }
     public boolean isFraudulentCustomer(Integer customerId){
         fraudCheckHistoryRepositry.save(
             FraudCheckHistory.builder()
